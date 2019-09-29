ce4970cd24a75c71e685f5824f66730db053e5d1
ksi77 <49089138+ksi77@users.noreply.github.com>
2019-09-29 11:26:06 +0300

    5. Личный проект: пока все дома

---
+++ js/main.js
@@ -0,0 +1,119 @@
+'use strict';
+// Напишите функцию для создания массива из 8 сгенерированных JS объектов. Каждый объект массива ‐ описание похожего объявления неподалёку. Структура объектов должна быть следующей:
+var OBJECT_COUNT = 8;
+
+var PHOTO_FILES = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
+var TIMES_OF_CHECK = ['12:00', '13:00', '14:00'];
+var TITLE = 'Предложение №';
+var TYPE_OF_HOUSE = ['palace', 'flat', 'house', 'bungalo'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var LETTERS = 'абвгдеёжзийклмнопрстуфхцчшщэюя   ';
+var AVATAR_FILE_PREFIX = 'img/avatars/user';
+var DESCRIPTION_LENGTH = 500;
+
+var createRandomString = function (letters, maxLength) {
+  var stringLength = getRandomNumberBetween(100, maxLength);
+  var string = '';
+  for (var i = 0; i < stringLength - 1; i++) {
+    string = string + letters.charAt(Math.round(Math.random() * letters.length));
+  }
+  string = string + '.';
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+var createFileList = function (fileTemplatePrefix, fileCount, fileExtention) {
+  var files = [];
+  for (var i = 0; i < fileCount; i++) {
+    files.push(fileTemplatePrefix + '0' + (i + 1) + fileExtention);
+  }
+  return files;
+};
+
+var getRandomElement = function (array, uniqueElement) {
+  var randomIndex = Math.round((array.length - 1) * Math.random());
+  var randomElement = array[randomIndex];
+  if (uniqueElement === true) {
+    array.splice(randomIndex, 1);
+  }
+  return randomElement;
+};
+
+var getRandomNumberBetween = function (minNumber, maxNumber) {
+  return Math.round(minNumber + Math.random() * (maxNumber - minNumber));
+};
+
+var getRandomArray = function (array, maxlength, uniqueElements) {
+  var arrayCopy = array;
+  var newArraylength = (uniqueElements === true || maxlength > arrayCopy.lengtn) ? getRandomNumberBetween(1, arrayCopy.length) : maxlength;
+  var newArray = [];
+  for (var i = 0; i < newArraylength; i++) {
+    newArray.push(getRandomElement(arrayCopy, uniqueElements));
+  }
+  return newArray;
+};
+
+var createObjectList = function (objectListLength) {
+  var objectList = [];
+  for (var i = 0; i < objectListLength; i++) {
+    objectList.push({
+      author: {
+        // Например, 01, 02 и т. д. Адреса изображений не повторяются
+        avatar: getRandomElement(avatarFiles, true)
+      },
+      offer: {
+        title: TITLE + (i + 1),
+        address: '' + getRandomNumberBetween(100, 999) + ', ' + getRandomNumberBetween(100, 999),
+        price: getRandomNumberBetween(200, 1000) + 'EUR',
+        type: getRandomElement(TYPE_OF_HOUSE),
+        rooms: getRandomNumberBetween(1, 10),
+        guests: getRandomNumberBetween(1, 10),
+        checkin: getRandomElement(TIMES_OF_CHECK),
+        checkout: getRandomElement(TIMES_OF_CHECK),
+        features: getRandomArray(FEATURES, FEATURES.length, true),
+        description: createRandomString(LETTERS, DESCRIPTION_LENGTH),
+        photos: getRandomArray(PHOTO_FILES, PHOTO_FILES.length, true)
+      },
+      location: {
+        x: getRandomNumberBetween(PIN_WIDTH / 2, BLOCK_WIDTH) - PIN_WIDTH / 2,
+        y: getRandomNumberBetween(130, 630) - PIN_HEIGHT
+      }
+    }
+    );
+  }
+  return objectList;
+};
+
+// На основе данных, созданных в первом пункте, создайте DOM-элементы, соответствующие меткам на карте, и заполните их данными из массива.
+// Итоговую разметку метки .map__pin можно взять из шаблона #pin.
+var pinsList = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content;
+var similarPin = pinTemplate.querySelector('.map__pin');
+var PIN_WIDTH = 50; // Не знаю, как получить из DOM
+var PIN_HEIGHT = 70;
+
+var mapBlock = document.querySelector('.map');
+mapBlock.classList.remove('map--faded');
+var BLOCK_WIDTH = mapBlock.scrollWidth;
+var avatarFiles = createFileList(AVATAR_FILE_PREFIX, 8, '.png');
+var objects = createObjectList(OBJECT_COUNT);
+
+var renderNewPin = function (object) {
+  var newPin = similarPin.cloneNode(true);
+  var newPinImg = newPin.querySelector('img');
+  newPinImg.src = object.author.avatar;
+  newPinImg.alt = object.author.title;
+  newPin.style.left = object.location.x + 'px';
+  newPin.style.top = object.location.y + 'px';
+  return newPin;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < objects.length; i++) {
+  fragment.appendChild(renderNewPin(objects[i]));
+}
+pinsList.appendChild(fragment);
